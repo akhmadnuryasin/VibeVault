c11bc6572a1ecb6d2ee65b1b050bb9901ef52eb5
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2024-02-26 03:56:45 +0700

    add music

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,6 +2,8 @@ import { v4 as uuidv4 } from "uuid";
 import thumbnailImage from "/images/thumbnail.jpg";
 import linkinParkAudio from "/music/Linki.mp3";
 import goodVibesAudio from "/music/good.mp3";
+import hapiAudio from "/music/hapy.mp3";
+import nfsAudio from "/music/nfs.mp3";
 import venceireAudio from "/music/venceire.mp3";
 
 function chillHop() {
@@ -21,6 +23,24 @@ function chillHop() {
       artist: "Artist Name",
       audio: goodVibesAudio,
       color: ["#FF5733", "#FFD700"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Happy Song",
+      cover: thumbnailImage,
+      artist: "Artist Name",
+      audio: hapiAudio,
+      color: ["#45CE30", "#FFD700"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Need for Speed",
+      cover: thumbnailImage,
+      artist: "Artist Name",
+      audio: nfsAudio,
+      color: ["#4D7FFF", "#FFD700"],
       id: uuidv4(),
       active: false,
     },
